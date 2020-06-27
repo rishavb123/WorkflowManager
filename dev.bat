bd72a1c6b390d3b94aacb6d401f410f29d93cd7d
Rishav Bhagat <rishavb123@gmail.com>
2020-06-27 14:44:11 -0400

    created dev script to quickly open everything needed to start development

---
+++ dev.bat
@@ -0,0 +1,7 @@
+@echo off
+cd workflow-manager
+start npm start
+cd ..
+chrome "https://console.firebase.google.com/u/2/project/rb-workflow-manager/overview" "https://github.com/rishavb123/WorkflowManager" "https://www.youtube.com/watch?v=5qap5aO4i9A"
+explorer .
+code .
