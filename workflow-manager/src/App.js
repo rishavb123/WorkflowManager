42834302e62fdb5aa90cf7cf37900822741bd6ee
Rishav Bhagat <rishavb123@gmail.com>
2020-06-27 13:11:15 -0400

    switched to class

--- workflow-manager/src/App.js
+++ workflow-manager/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            
-        </div>
-    );
+class App extends React.Component {
+    
+    render() {
+        return (
+            <div className="App">
+                Hello World
+            </div>
+        );
+    }
+
 }
 
 export default App;
